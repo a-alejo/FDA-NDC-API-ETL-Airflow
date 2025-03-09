6b08cff6c36d85a5915a443a72f6a369f7dd532a
a-alejo <alfredo3xa@gmail.com>
2025-03-09 17:13:01 -0400

    chore: format and lint all files

--- dags/etl_dag.py
+++ dags/etl_dag.py
@@ -6,38 +6,30 @@ from src.load import load_data
 from src.transform import transform_data
 
 dag = DAG(
-    dag_id='etl_pipeline',
-    description='ETL pipeline that extracts FDA NDC data and loads to Postgres DB',
+    dag_id="etl_pipeline",
+    description="ETL pipeline that extracts FDA NDC data and loads to Postgres DB",
     start_date=datetime(2025, 2, 15),
-    schedule='@daily',
+    schedule="@daily",
     default_args={
-        'owner': 'Alfredo_Test',
-        'retries': 1,
-        'retry_delay': timedelta(minutes=5),
+        "owner": "Alfredo_Test",
+        "retries": 1,
+        "retry_delay": timedelta(minutes=5),
     },
     catchup=False,
 )
 
 # Task 1: Extract data
 extract_task = PythonOperator(
-    task_id='extract_data',
-    python_callable=extract_data,
-    dag=dag
+    task_id="extract_data", python_callable=extract_data, dag=dag
 )
 
 # Task 2: Transform data
 transform_task = PythonOperator(
-    task_id='transform_data',
-    python_callable=transform_data,
-    dag=dag
+    task_id="transform_data", python_callable=transform_data, dag=dag
 )
 
 # Task 3: Load data
-load_task = PythonOperator(
-    task_id='load_data',
-    python_callable=load_data,
-    dag=dag
-)
+load_task = PythonOperator(task_id="load_data", python_callable=load_data, dag=dag)
 
 # Set task dependencies
 extract_task >> transform_task >> load_task
--- src/load.py
+++ src/load.py
@@ -5,63 +5,65 @@ from config import db_user, db_password
 
 def load_data():
     conn = psycopg2.connect(
-        host = 'localhost',
-        database = 'Postgres 16 - Localhost - FDA-NDC-ETL_db',
-        user = db_user,
-        password = db_password)
+        host="localhost",
+        database="Postgres 16 - Localhost - FDA-NDC-ETL_db",
+        user=db_user,
+        password=db_password,
+    )
 
     cursor = conn.cursor()
 
     # Using a manual approach for data extraction and handling to break out each item for SQL insertion instead of relying on pd.json_normalize
     for result in all_results:
-        product_ndc = result.get('product_ndc')
-        generic_name = result.get('generic_name')
-        labeler_name = result.get('labeler_name')
-        brand_name = result.get('brand_name')
-        active_ingredients = json.dumps(result.get('active_ingredients'))  # this is a dictionary
-        finished = result.get('finished')
-        packaging = json.dumps(result.get('packaging'))  # this is a dictionary
-        listing_expiration_date = result.get('listing_expiration_date')
-        openfda = json.dumps(result.get('openfda')) # this is a dictionary
-        marketing_category = result.get('marketing_category')
-        dosage_form = result.get('dosage_form')
-        spl_id = result.get('spl_id')
-        product_type = result.get('product_type')
-        route = result.get('route')
-        marketing_start_date = result.get('marketing_start_date')
-        product_id = result.get('product_id')
-        application_number = result.get('application_number')
-        brand_name_base = result.get('brand_name_base')
-        pharm_class = result.get('pharm_class') # this is a dictionary
-
+        product_ndc = result.get("product_ndc")
+        generic_name = result.get("generic_name")
+        labeler_name = result.get("labeler_name")
+        brand_name = result.get("brand_name")
+        active_ingredients = json.dumps(
+            result.get("active_ingredients")
+        )  # this is a dictionary
+        finished = result.get("finished")
+        packaging = json.dumps(result.get("packaging"))  # this is a dictionary
+        listing_expiration_date = result.get("listing_expiration_date")
+        openfda = json.dumps(result.get("openfda"))  # this is a dictionary
+        marketing_category = result.get("marketing_category")
+        dosage_form = result.get("dosage_form")
+        spl_id = result.get("spl_id")
+        product_type = result.get("product_type")
+        route = result.get("route")
+        marketing_start_date = result.get("marketing_start_date")
+        product_id = result.get("product_id")
+        application_number = result.get("application_number")
+        brand_name_base = result.get("brand_name_base")
+        pharm_class = result.get("pharm_class")  # this is a dictionary
 
         # Add data to the table
         cursor.execute(
-            '''
+            """
             INSERT INTO FDA_Drugs_db (
-            "product_ndc",
-            "generic_name",
-            "labeler_name",
-            "brand_name",
-            "active_ingredients",
-            "finished",
-            "packaging",
-            "listing_expiration_date",
-            "openfda",
-            "marketing_category",
-            "dosage_form",
-            "spl_id",
-            "product_type",
-            "route",
-            "marketing_start_date",
-            "product_id",
-            "application_number",
-            "brand_name_base",
-            "pharm_class"
-        )
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-        ON CONFLICT ("product_id") DO NOTHING
-            ''', 
+                "product_ndc",
+                "generic_name",
+                "labeler_name",
+                "brand_name",
+                "active_ingredients",
+                "finished",
+                "packaging",
+                "listing_expiration_date",
+                "openfda",
+                "marketing_category",
+                "dosage_form",
+                "spl_id",
+                "product_type",
+                "route",
+                "marketing_start_date",
+                "product_id",
+                "application_number",
+                "brand_name_base",
+                "pharm_class"
+                )
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                ON CONFLICT ("product_id") DO NOTHING
+            """,
             (
                 product_ndc,
                 generic_name,
@@ -81,8 +83,8 @@ def load_data():
                 product_id,
                 application_number,
                 brand_name_base,
-                pharm_class
-            )
+                pharm_class,
+            ),
         )
 
         # Commit the transaction
